fdb40f4029156b0c37e28024e87766426a6b98ff
Muazzin <muazzin2009@gmail.com>
2025-05-02 01:20:42 +0500

    tested the API gonna work on the frontend now

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [data, setData] = useState(""); 
+const url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/3';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '210cf12d9amsh8f8abf948ad23d8p1ea06bjsne581f4855db6',
+		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+	}
+};
+useEffect(() => {
+  fetch(url, options)
+  .then(response => response.json())
+  .then(response => setData(response))
+  .catch(err => console.error(err));
+})
 
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="size-60" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="size-60" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className='transition-all duration-1000 hover:-translate-y-1' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {data.title}
     </>
   )
 }
