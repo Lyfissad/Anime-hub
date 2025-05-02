2cf2a6f54ebea45e8da42f6ecc1da0d66ebaa2ad
Muazzin <muazzin2009@gmail.com>
2025-05-03 03:29:12 +0500

    got shadcn set up done and working on header now

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,31 @@
 import { SlMenu } from "react-icons/sl";
-
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+  
 
 
 export default function Header(){
     return(
-        <div className="header">
-            <SlMenu className = "fill-amber-950"/>
+        <div className="flex bg-red-950 h-16 p-4">
+            <Drawer className = "xs:visible md:invisible">
+            <DrawerTrigger><SlMenu className = "fill-amber-50 size-8"/></DrawerTrigger>
+            <DrawerContent className="h-[60%]">
+                <ul className="space-y-4">
+                    <li>New</li>
+                    <li>Popular</li>
+                    <li>browse all</li>
+                </ul>
+            </DrawerContent>
+            </Drawer>
+            
         </div>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+      extend: {
+        colors:{
+            black: "#23252b",
+            yellow: "#008000"
+        }
+      },
+    },
+    plugins: [],
+  }
+  
