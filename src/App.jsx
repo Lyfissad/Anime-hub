8853d6c9fb8f3afbc1c43233ce569d8c399d2eaf
Muazzin <muazzin2009@gmail.com>
2025-05-02 18:20:24 +0500

    Api and mapping logic understood working frontend now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
 import { useEffect, useState } from 'react';
 import './App.css'
+import Header from './components/header';
 
 function App() {
-const [data, setData] = useState(""); 
-const url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/3';
+const [data, setData] = useState({});
+const viteApiKey = import.meta.env.VITE_API_KEY
+const viteApiUrl = import.meta.env.VITE_API_URL 
+const url = `${viteApiUrl}/anime?page=1&size=10&sortBy=ranking&sortOrder=asc`;
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '210cf12d9amsh8f8abf948ad23d8p1ea06bjsne581f4855db6',
+		'x-rapidapi-key': viteApiKey,
 		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
 	}
 };
-useEffect(() => {
-  fetch(url, options)
-  .then(response => response.json())
-  .then(response => setData(response))
-  .catch(err => console.error(err));
-})
 
-  
-  return (
-    <>
-    {data.title}
-    </>
-  )
+// useEffect(() => {
+//   fetch(url, options)
+//   .then(response => response.json())
+//   .then(response => {
+//     console.log(response);
+//     setData(response);
+//   })
+//   .catch(err => console.error(err));  
+// },[])
+
+console.log(data)
+return (
+  <Header />
+);
+
 }
 
 export default App
+
+
+//Fetching and mapping logic
+// <>
+//     <h1>Anime List Bitch</h1>
+//     {data? data.data.map((item) => ( 
+//     <div className="container">
+//       <h1>{item.title}</h1>
+//       <p>{item.synopsis}</p>
+//       <img src={item.image} alt="{item.title}" />
+//     </div>
+//   )
+// )
+// : <h1>Loading</h1>
+// }
+//   </>
+
+
+
+
+
+
+// {data && data.data ? (
+//   data.data.map((item) => (
+//     <div className="container" key={item.id}>
+//       <h2>{item.title}</h2>
+//       <p>{item.synopsis}</p>
+//       <img src={item.image} alt={item.title} />
+//     </div>
+//   ))
+// ) : (
+//   <p>Loading...</p>
+// )}
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import { SlMenu } from "react-icons/sl";
+
+
+
+export default function Header(){
+    return(
+        <div className="header">
+            <SlMenu className = "fill-amber-950"/>
+        </div>
+    )
+}
